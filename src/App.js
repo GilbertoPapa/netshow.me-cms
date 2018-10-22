e9265eb7158cc6bac4ca37b7f3ed5eb195348007
Gilberto Papa <Gilbertopapa013@hotmail.com>
2018-10-22 16:39:45 -0300

    construção do dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   render() {
