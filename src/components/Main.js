f1b1e87130e62216f6b3b1436f68982cd5033f4b
Vinicius Costa <vinicius.costa89@gmail.com>
2018-10-26 00:19:05 -0300

    Página Transmissão

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,11 @@ import { Switch, Route } from "react-router-dom"
 import Dashboard from "./dashboard/Dashboard";
 import Perfil from "./admin/Perfil";
 import Usuarios from "./admin/GerenciamentoDeUsuarios";
+import Transmissao from "./transmissao/Transmissao";
 import Galeria from "./album/Album";
 import Analytics from "./analytics/Analytics";
 import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
-
-// The Main component renders one of the three provided
-// Routes (provided that one matches). Both the /roster
-// and /schedule routes will match any pathname that starts
-// with /roster or /schedule. The / route will only match
-// when the pathname is exactly the string "/"
-
-// const drawerWidth = 240;
 
 const styles = theme => ({
   content: {
@@ -26,14 +19,7 @@ const styles = theme => ({
     }),
     margin: "80px auto 0",
     maxWidth: "1280px"
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  }
+  }  
 });
 
 class Main extends React.Component {
@@ -52,6 +38,7 @@ class Main extends React.Component {
           <Route exact path="/" component={Dashboard} />
           <Route path="/admin/perfil" component={Perfil} />
           <Route path="/admin/gerenciamento-de-usuarios" component={Usuarios} />
+          <Route path="/transmissao" component={Transmissao} />
           <Route path="/galeria" component={Galeria} />
           <Route path="/analytics/video" component={Analytics} />
         </Switch>
